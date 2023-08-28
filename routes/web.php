c54a405d436d48c0b5790d7b1368e9d395ddf2a8
AdibFirmansyah <adibfirmansyah27@gmail.com>
2023-08-29 00:15:49 +0700

    add profile

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,18 @@ Route::get('/', function () {
 });
 
 // AUTH ROUTER CONTROLLER
-Route::controller(AuthController::class)->group(function() {
-    Route::get('/auth','index')->middleware('isGuest')->name('login');
-    Route::post('/auth/login','loginGan')->middleware('isGuest')->name('postlogin');
-    Route::get('/auth/register','register')->middleware('isGuest')->name('register');
-    Route::post('/auth/registme','createUser')->middleware('isGuest')->name('postregister');
-    Route::get('/auth/logout','logout')->name('logout');
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/auth', 'index')->middleware('isGuest')->name('login');
+    Route::post('/auth/login', 'loginGan')->middleware('isGuest')->name('postlogin');
+    Route::get('/auth/register', 'register')->middleware('isGuest')->name('register');
+    Route::post('/auth/registme', 'createUser')->middleware('isGuest')->name('postregister');
+    Route::get('/auth/logout', 'logout')->name('logout');
 });
 
 // PAGE CONTROLLER
-Route::controller(pageController::class)->group(function() {
+Route::controller(pageController::class)->group(function () {
     Route::get('/dashboard', 'dashboard')->middleware('isUser')->name('dashboard');
     Route::get('/docs', 'docs')->middleware('isUser')->name('docs');
     Route::get('/profil', 'profil')->middleware('isUser')->name('profil');
+    Route::post('/profil/update', 'update')->middleware('isUser')->name('profile.update');
 });
