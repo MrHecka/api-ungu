aefe488d94eeaf1320fd6a1a87e2b183ae46c5bd
MrHecka <ejetz99@gmail.com>
2023-08-31 20:21:51 +0700

    update wikipediaController

--- app/Http/Controllers/API/wikipediaController.php
+++ app/Http/Controllers/API/wikipediaController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use DOMDocument;
 use DOMXPath;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
 
 class wikipediaController extends Controller
@@ -16,30 +17,39 @@ class wikipediaController extends Controller
      */
     public function index(Request $request)
     {
-        $apikeyheaders = $request->header('apikey');
-        $userApiKey = User::where('apikey', $apikeyheaders)->first();
-        $httpreq = new Client();
-        $reqapi = $httpreq->request('GET', 'https://id.wikipedia.org/wiki/Wikipedia:Tahukah_Anda');
-        $htmlString = (string) $reqapi->getBody();
-        libxml_use_internal_errors(true);
-        $doc = new DOMDocument();
-        $doc->loadHTML($htmlString);
-        $xpath = new DOMXPath($doc);
-        $getText = $xpath->evaluate('//div[@class="mw-parser-output"]//li');
-        $arrayText = [];
-        foreach ($getText as $texts) {
-            if (preg_match('/(?<=\... ).*/', $texts->textContent.PHP_EOL, $group)) {
-                $arrayText[] = $group[0];
+        try {
+            $apikeyheaders = $request->header('apikey');
+            $userApiKey = User::where('apikey', $apikeyheaders)->first();
+            $httpreq = new Client();
+            $reqapi = $httpreq->request('GET', 'https://id.wikipedia.org/wiki/Wikipedia:Tahukah_Anda');
+            $htmlString = (string) $reqapi->getBody();
+            libxml_use_internal_errors(true);
+            $doc = new DOMDocument();
+            $doc->loadHTML($htmlString);
+            $xpath = new DOMXPath($doc);
+            $getText = $xpath->evaluate('//div[@class="mw-parser-output"]//li');
+            $arrayText = [];
+            foreach ($getText as $texts) {
+                if (preg_match('/(?<=\... ).*/', $texts->textContent.PHP_EOL, $group)) {
+                    $arrayText[] = $group[0];
+                }
             }
-        }
 
-        return response()->json([
-            'pesan'=>'sukses',
-            'status'=>200,
-            'source' => 'https://id.wikipedia.org/wiki/Wikipedia:Tahukah_Anda',
-            'nama_apikey'=>$userApiKey->nama,
-            'data'=>$arrayText]
-            , 200);
+            return response()->json([
+                'pesan'=>'sukses',
+                'status'=>200,
+                'source' => 'https://id.wikipedia.org/wiki/Wikipedia:Tahukah_Anda',
+                'nama_apikey'=>$userApiKey->nama,
+                'data'=>$arrayText]
+                , 200);
+        } catch (ClientException $err) {
+            return response()->json([
+                'pesan'=>'gagal',
+                'status'=>404,
+                'nama_apikey'=>$userApiKey->nama,
+                'error'=>'Something went wrong | '.$err->getResponse()->getBody().' | '.$err->getResponse()->getStatusCode()
+            ], 404);
+        }
     }
 
     /**
