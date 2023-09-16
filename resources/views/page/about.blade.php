51abfa24869a96c73dee02711ab3f30e54f22972
Dilla-Novyanti <dilanovyanti@gmail.com>
2023-09-16 23:06:31 +0700

    fix gambar penyet

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -6,13 +6,17 @@
 
 @section('isikonten')
 
-    <div class="overflow-y-auto relative flex md:h-screen sm:w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" style="background-image:url({{url('img/bgAuth.webp')}})">
-        <div class="grid lg:grid-cols-3 gap-4">
+    <div class="overflow-y-auto relative flex md:h-screen pt-16 md:pt-16 sm:w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" style="background-image:url({{url('img/bgAuth.webp')}})">
+        <div class="grid lg:grid-cols-3 gap-4 pt-16 md:pt-16">
 
         @foreach($contributors as $contrib)
             <div class="overflow-y-auto max-[1024px]:h-120 max-[1024px]:py-2 rounded-xl bg-gray-800 bg-opacity-50 px-16 py-5 shadow-lg backdrop-blur-md max-sm:px-8">
                     <div class="p-5 text-center text-white max-w-sm">
-                        <a href="{{ url($contrib['image']) }}" target="_blank"><img class="w-32 h-32 rounded-full mx-auto hover:scale-150" src="{{ url($contrib['image']) }}" alt=""></a>
+                        <a href="{{ url($contrib['image']) }}" target="_blank">
+                            <div class="w-32 h-32 rounded-full mx-auto hover:scale-150">
+                                <img src="{{ url($contrib['image']) }}" class="rounded-full object-cover w-full h-full" alt="">
+                            </div>
+                        </a>
                         <div class="text-sm mt-5">
                             <a href="#"
                                 class="font-medium leading-none text-white hover:text-indigo-600 transition duration-500 ease-in-out">{{ $contrib['nama'] }}
